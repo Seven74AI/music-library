2e217b9627773aad41546c691f7e36bc164fbdf0
root <sevenai@agentmail.to>
2026-07-16 15:39:35 +0200

    fix: use wildcard path for /resources/images MSW handler

--- tests/mocks/image-optimizer.ts
+++ tests/mocks/image-optimizer.ts
@@ -5,9 +5,9 @@ const PIXEL =
 	'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=='
 
 export const handlers = [
-	http.get('/resources/images', ({ request }) => {
-		// Matches /resources/images and /resources/images?src=...&w=...&h=...
-		// regardless of query parameters
+	http.get('*/resources/images', () => {
+		// Wildcard matches both relative (/resources/images) and absolute
+		// (http://localhost:3000/resources/images?src=...) URLs
 		return new HttpResponse(Buffer.from(PIXEL, 'base64'), {
 			headers: { 'Content-Type': 'image/png' },
 		})
